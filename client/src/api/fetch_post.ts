cb6b227c118325942dfe378f21587bfdae98fc1b
katiek78 <kermodekatie@gmail.com>
2023-02-23 17:23:07 +0000

    refactor(fetch_post): Refactored fetch_post

--- client/src/api/fetch_post.ts
+++ client/src/api/fetch_post.ts
@@ -2,9 +2,8 @@ import { baseUrl } from "./base_url";
 
 export async function fetchPost(id: string) {
 	try {
-		const result = await fetch(baseUrl + "/api/posts/" + id);
-		const post = await result.json();
-		return post;
+		const result: Response = await fetch(baseUrl + "/api/posts/" + id);
+		return result.json();		
 	} catch {
 		return [];
 	}
